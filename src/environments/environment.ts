1742d9ad56808b43d8a2fd25d1b6c3756ca1d15f
JasonLee <lidechao@beibeiyue.com>
2019-05-23 16:23:12 +0800

    '合并'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,10 @@
 export const environment = {
   production: false,
   version: '0.0.0',
+
   domain: 'http://192.168.1.115:8855/first_light/kindergarten',//本地
-  // domain: 'http://39.107.232.95:6005/first_light/kindergarten',
-   //domain: 'http://chuzg.beibeiyue.com/first_light/kindergarten'
+  //domain: 'http://39.107.232.95:6005/first_light/kindergarten',
+
 };
 
 /*
