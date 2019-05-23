bf93ee9b7f542e942d695f46e2a01eb8726b4f8c
phuhoang <phuhoang@sina.com>
2019-05-23 21:03:18 +0800

    合并前存储

--- src/app/modules/visit/public/update/update.component.ts
+++ src/app/modules/visit/public/update/update.component.ts
@@ -1,12 +1,7 @@
 import { HttpService } from 'src/app/ng-relax/services/http.service';
-import { CacheService } from '../../../../ng-relax/services/cache.service';
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { NzMessageService, NzDrawerRef } from 'ng-zorro-antd';
-import { DatePipe } from '@angular/common';
-import { MonthdiffPipe } from '../../../../ng-relax/pipes/monthdiff.pipe';
-import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NzDrawerRef } from 'ng-zorro-antd';
 import { DrawerClose } from 'src/app/ng-relax/decorators/drawer/close.decorator';
 import { DrawerSave } from 'src/app/ng-relax/decorators/drawer/save.decorator';
 import { ControlValid } from 'src/app/ng-relax/decorators/form/valid.decorator';
@@ -30,8 +25,9 @@ export class UpdateComponent implements OnInit {
 
   ngOnInit() {
     this.formGroup = this.fb.group({
-      nick: [],                                                                               // 宝宝姓名
-      mobilePhone: [, [Validators.required, Validators.pattern(/^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/)], [this._parentPhoneAsyncValidator]]
+      id: [this.id],
+      studentName: [, [Validators.required]],                                                                               // 宝宝姓名
+      mobilePhone: [, [Validators.required, Validators.pattern(/^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/)]]
     });
   }
   
@@ -40,22 +36,8 @@ export class UpdateComponent implements OnInit {
   @ControlValid() valid: (key, type?) => boolean;
 
   saveLoading: boolean;
-  @DrawerSave('') save: () => void;
+  @DrawerSave('/membermanage/clue/saveClue') save: () => void;
   
-  
-  _parentPhoneAsyncValidator = (control: FormControl): any => {
-    return Observable.create(observer => {
-      let params: any = { mobilePhone: this.formGroup.get('mobilePhone').value };
-      if (this.id) { params.id = this.id; }
-      this.http.post('/common/checkTelphoneNum', { paramJson: JSON.stringify(params) }, false).then(res => {
-        observer.next(res.result ? null : { error: true, duplicated: true });
-        observer.complete();
-      }, err => {
-        observer.next(null);
-        observer.complete();
-      })
-    })
-  };
   
 
 }
