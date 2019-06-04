13e422f23b71b450027eb220d29234bfccd9918f
phuhoang <phuhoang@sina.com>
2019-06-04 17:07:29 +0800

    修复卡项bug

---
+++ src/app/modules/index/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.less']
+})
+export class ListComponent implements OnInit {
+
+  @Input() url: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
