bfb18e93f92eeb33e6a50aa2a2b1b8956b50b81d
phuhoang <phuhoang@sina.com>
2019-06-04 21:04:26 +0800

    初之光三期-beta定稿

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   version: '0.0.1',
-  domain: 'http://chuzg.beibeiyue.com/first_light/kindergarten',
-  domainEs: 'http://192.168.1.243:8080/es/',
+  domain: 'http://chuzg.haochengzhang.com/first_light/kindergarten',
+  domainEs: 'http://es.zhaochengzhang.com/es/',
 };
