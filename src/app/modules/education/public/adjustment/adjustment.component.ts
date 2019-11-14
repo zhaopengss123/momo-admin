cf8aec7f9631e844ef492fb92e59d8f9330939de
zhaopeng <874391057@qq.com>
2019-11-14 19:04:57 +0800

    合并前准备

---
+++ src/app/modules/education/public/adjustment/adjustment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adjustment',
+  templateUrl: './adjustment.component.html',
+  styleUrls: ['./adjustment.component.less']
+})
+export class AdjustmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
